e6f94ab305405a55b22a256ca619548751ee8a55
garrysjh <garrysjh@gmail.com>
2024-02-29 00:05:03 +0800

    made themes work, singular button

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import "./App.css";
+import { useState } from 'react'
+import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <NavigationMenuLink>Link</NavigationMenuLink>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-      <Button>Test</Button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and e to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <Button>Click me</Button>
+    </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
