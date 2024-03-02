64067e88a29434f2304627816b55cad4aff352af
garrysjh <garrysjh@gmail.com>
2024-03-03 01:00:51 +0800

    added sample text, basic components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,29 @@ import React from "react";
 import { Button } from "@/components/ui/button";
 import { NavigationBar } from "@/components/navigation-bar";
 import { ModeToggle } from "@/components/mode-toggle";
-
+import { ResizableHero } from "@/components/resizable-hero";
+ 
 export default function Home() {
   return (
-    <main>
-      <div className="flex md:flex md:flex-grow flex-row-reverse space-x-1 p-8">
+    <main className="overflow-hidden">
+      <div className="flex md:flex md:flex-grow flex-row-reverse space-x-1 p-8 max-w-full">
         <div className="row">
           <ModeToggle />
         </div>
         <div className="row">
           <NavigationBar />
         </div>
-        
       </div>
-      <div className="min-h-screen flex-col items-center justify-between p-20 container col-auto">
+      <div className="min-h-max flex-col items-center justify-between pl-20 pr-20 container col-auto">
+      <h1 className="scroll-m-20 text-4xl font-semibold tracking-tight lg:text-5xl italic">
+      Hi, I'm Garry Shi.
+    </h1>
+    <h4 className="scroll-m-20 text-xl tracking-tight pb-4 pt-4">
+      I'm a work-in-progress. Just like this website.
+    </h4>
         <div className="z-10 max-w-1xl w-full items-center justify-between text-md lg:flex">
+    
+    
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
           suscipit sapien sed neque consectetur molestie. Praesent tempor eros
           sit amet lectus interdum, eget posuere ligula aliquam. Integer feugiat
@@ -32,44 +40,9 @@ export default function Home() {
           ornare sapien non mollis. Vestibulum arcu sapien, ultrices id sagittis
           a, imperdiet id est. Mauris tincidunt mauris id risus lacinia, eu
           suscipit enim aliquet. Integer porttitor dui a bibendum aliquam. Duis
-          iaculis non nisl ut iaculis. Curabitur iaculis enim in tristique
-          gravida. Vivamus diam sem, cursus eget sodales ac, hendrerit eu risus.
-          Donec maximus massa tellus, at finibus elit molestie et. Pellentesque
-          pharetra egestas ornare. Vestibulum consequat eros turpis, sed
-          lobortis orci rutrum in. Proin quis ante tortor. Mauris ultricies
-          magna vehicula nisl fringilla, vitae porttitor turpis suscipit.
-          Aliquam malesuada magna eu mauris dapibus volutpat nec et ante.
-          Maecenas tincidunt libero eget condimentum consequat. Etiam mattis
-          blandit mi, et tristique lacus. Vivamus vehicula erat eu diam
-          sollicitudin, eu cursus augue fringilla. Aliquam pulvinar facilisis
-          tellus vitae volutpat. Integer vitae leo sodales, consectetur nunc ac,
-          venenatis ex. Ut sit amet ultrices quam, et eleifend leo. Nunc
-          pellentesque auctor ullamcorper. Ut imperdiet metus nec libero
-          ullamcorper, vitae ullamcorper risus hendrerit. Maecenas iaculis
-          posuere convallis. Morbi ac bibendum neque. Vestibulum id lacinia
-          augue. Morbi sit amet suscipit enim. Nunc pulvinar arcu nisi, non
-          luctus est elementum at. Mauris posuere ut eros nec imperdiet. In
-          posuere eu arcu sit amet aliquet. Curabitur libero quam, tempus vel
-          magna sit amet, bibendum suscipit lacus. Praesent non faucibus mauris.
-          Mauris tincidunt mauris vel libero egestas, eu sagittis odio posuere.
-          Proin non sagittis purus, sit amet sodales lectus. Nam pretium et nisl
-          efficitur vehicula. Vestibulum finibus enim eget ligula lobortis, sed
-          congue tellus dignissim. Mauris arcu tellus, rhoncus id lacus nec,
-          rutrum convallis nisi. Fusce eu diam vel ante interdum efficitur ut a
-          risus. Praesent ultricies, velit non tincidunt vulputate, orci nisl
-          suscipit tellus, vehicula tempus ligula odio a felis. Aliquam
-          tristique augue eget dui gravida, sit amet placerat arcu luctus. Nunc
-          ante leo, ultricies a risus vitae, pretium tincidunt nisl. Nunc
-          interdum sem eget pharetra sodales. Praesent semper ipsum at lacinia
-          auctor. Maecenas nec enim facilisis mi mollis venenatis sit amet quis
-          ex. Donec non nibh blandit, tincidunt nisi id, luctus ligula. Nulla
-          rhoncus tortor vitae tortor tempor elementum. Nunc eget nunc id mauris
-          hendrerit tristique. Sed ac venenatis leo. Nullam nec tellus erat.
-          Aliquam erat volutpat. Vivamus mollis quam pretium, tempus diam sit
-          amet, rhoncus sem.
+          iaculis non nisl ut iaculis. 
         </div>
         <div className="text-center">
-          <Button>Test bUtton</Button>
         </div>
       </div>
     </main>
